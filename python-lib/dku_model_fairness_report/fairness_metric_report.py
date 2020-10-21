f92ef88dfeb07e2d003aa0ea43e8513d5b3cc109
Du Phan <du.phan@dataiku.com>
2020-10-21 11:41:08 +0200

    fix bug when there is only 1 value in y_true, y_pred for a population

--- python-lib/dku_model_fairness_report/fairness_metric_report.py
+++ python-lib/dku_model_fairness_report/fairness_metric_report.py
@@ -27,14 +27,8 @@ class ModelFairnessMetric(object):
             ]
 
     @staticmethod
-    def _compute_confusion_matrix_metrics(y_true, y_pred, advantageous_outcome, sample_weight=None):
-        label_list = [x for x in np.unique(y_true) if x != advantageous_outcome]
-        label_list.append(advantageous_outcome)
+    def _compute_confusion_matrix_metrics(y_true, y_pred, label_list, sample_weight=None):
         conf_matrix = confusion_matrix(y_true, y_pred, labels=label_list, sample_weight=sample_weight)
-        print('))))))')
-        print(conf_matrix)
-        print('------')
-        print(conf_matrix.shape)
 
         true_negative = conf_matrix[0][0]
         false_negative = conf_matrix[1][0]
@@ -44,29 +38,31 @@ class ModelFairnessMetric(object):
         return true_negative, false_negative, true_positive, false_positive
 
     @staticmethod
-    def demographic_parity(y_true, y_pred, advantageous_outcome, sample_weight=None):
+    def demographic_parity(y_true, y_pred, label_list, sample_weight=None):
         """
         demographic_parity just care about y_pred, but we keep y_true to have a homogene api
         """
-        return np.round(np.sum(y_pred == advantageous_outcome, dtype=float) / len(y_pred), DkuFairnessConstants.NUMBER_OF_DECIMALS)
+
+        # last label is the advantageous one
+        return np.round(np.sum(y_pred == label_list[-1], dtype=float) / len(y_pred), DkuFairnessConstants.NUMBER_OF_DECIMALS)
 
     @staticmethod
-    def equality_of_opportunity(y_true, y_pred, advantageous_outcome, sample_weight=None):
-        true_negative, false_negative, true_positive, false_positive = ModelFairnessMetric._compute_confusion_matrix_metrics(y_true, y_pred, advantageous_outcome, sample_weight)
+    def equality_of_opportunity(y_true, y_pred, label_list, sample_weight=None):
+        true_negative, false_negative, true_positive, false_positive = ModelFairnessMetric._compute_confusion_matrix_metrics(y_true, y_pred, label_list, sample_weight)
         # Sensitivity, hit rate, recall, or true positive rate
         true_positive_rate = np.round(true_positive / (true_positive + false_negative), DkuFairnessConstants.NUMBER_OF_DECIMALS)
         return true_positive_rate
 
     @staticmethod
-    def equalized_odds(y_true, y_pred, advantageous_outcome, sample_weight=None):
-        true_negative, false_negative, true_positive, false_positive = ModelFairnessMetric._compute_confusion_matrix_metrics(y_true, y_pred, advantageous_outcome, sample_weight)
+    def equalized_odds(y_true, y_pred, label_list, sample_weight=None):
+        true_negative, false_negative, true_positive, false_positive = ModelFairnessMetric._compute_confusion_matrix_metrics(y_true, y_pred, label_list, sample_weight)
         true_positive_rate = np.round(true_positive / (true_positive + false_negative), DkuFairnessConstants.NUMBER_OF_DECIMALS)
         false_positive_rate = np.round(false_positive / (true_negative + false_positive), DkuFairnessConstants.NUMBER_OF_DECIMALS)
         return true_positive_rate, false_positive_rate
 
     @staticmethod
-    def predictive_rate_parity(y_true, y_pred, advantageous_outcome, sample_weight=None):
-        true_negative, false_negative, true_positive, false_positive = ModelFairnessMetric._compute_confusion_matrix_metrics(y_true, y_pred, advantageous_outcome, sample_weight)
+    def predictive_rate_parity(y_true, y_pred, label_list, sample_weight=None):
+        true_negative, false_negative, true_positive, false_positive = ModelFairnessMetric._compute_confusion_matrix_metrics(y_true, y_pred, label_list, sample_weight)
         # Precision or positive predictive value
         positive_predictive_value = np.round(true_positive / (true_positive + false_positive), DkuFairnessConstants.NUMBER_OF_DECIMALS)
         return positive_predictive_value
@@ -81,6 +77,8 @@ class ModelFairnessMetricReport(object):
         self.sample_weight = sample_weight  # 1D array
         self._check()
 
+        self.label_list = self._get_label_list()
+
     def _check(self):
         possible_outcomes = set(self.y_true.unique()).union(set(self.y_pred.unique()))
         if self.advantageous_outcome is not None and self.advantageous_outcome not in possible_outcomes:
@@ -93,6 +91,12 @@ class ModelFairnessMetricReport(object):
         if reference_group not in summary.get(DkuFairnessConstants.BY_GROUP).keys():
             raise ValueError('The chosen reference group "{0}" does not exist in the input metric summary.'.format(reference_group))
 
+    def _get_label_list(self):
+        label_list = [x for x in np.unique(self.y_true) if x != self.advantageous_outcome]
+        label_list.append(self.advantageous_outcome)
+
+        return label_list
+
     def compute_metric_per_group(self, metric_function):
         """
         metric_function is a function with signature metric_function(y_true, y_pred, *metric_params)
@@ -101,7 +105,7 @@ class ModelFairnessMetricReport(object):
                              self.y_true,
                              self.y_pred,
                              sensitive_features=self.sensitive_feature_values,
-                             advantageous_outcome=self.advantageous_outcome)
+                             label_list=self.label_list)
 
     def _get_reference_group(self, reference_group, summary):
         # if there is no reference group, take the overall metric
