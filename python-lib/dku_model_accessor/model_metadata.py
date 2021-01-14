9ecd469163a75b53b3545b0e29f6392f635b91e5
Du Phan <du.phan@dataiku.com>
2021-01-14 15:51:27 +0100

    update wording

--- python-lib/dku_model_accessor/model_metadata.py
+++ python-lib/dku_model_accessor/model_metadata.py
@@ -4,11 +4,16 @@ import os
 import sys
 from dataiku.doctor.posttraining.model_information_handler import PredictionModelInformationHandler
 
-
 def get_model_handler(model, version_id=None):
     saved_model_version_id = _get_saved_model_version_id(model, version_id)
     return _get_model_info_handler(saved_model_version_id)
 
+def _get_saved_model_version_id(model, version_id=None):
+    model_def = model.get_definition()
+    if version_id is None:
+        version_id = model_def.get('activeVersion')
+    saved_model_version_id = 'S-{0}-{1}-{2}'.format(model_def.get('projectKey'), model_def.get('id'), version_id)
+    return saved_model_version_id
 
 def _get_model_info_handler(saved_model_version_id):
     infos = saved_model_version_id.split("-")
@@ -46,11 +51,3 @@ def _get_model_info_handler(saved_model_version_id):
             raise_(Exception, "The plugin does not support Clustering model.", sys.exc_info()[2])
         else:
             raise_(Exception, "Fail to load saved model: {}".format(e), sys.exc_info()[2])
-
-
-def _get_saved_model_version_id(model, version_id=None):
-    model_def = model.get_definition()
-    if version_id is None:
-        version_id = model_def.get('activeVersion')
-    saved_model_version_id = 'S-{0}-{1}-{2}'.format(model_def.get('projectKey'), model_def.get('id'), version_id)
-    return saved_model_version_id
